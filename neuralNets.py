e548decaf50f99f8f302ef61b35aec48ee5108fc
DimuthRuwantha <dimuthruwantha@gmail.com>
2017-08-10 18:03:04 +0530

    modifications on Neural network

--- neuralNets.py
+++ neuralNets.py
@@ -1,9 +1,80 @@
 import numpy as np
+import prettyprint
 from testing.Testing import Testing
 
 from training.TrainingNetwork import TrainingNetwork
+import util
+
+
+def get_matrices(training_set=[]):
+
+    matrix_list = []
+    output_list = []
+    for lst in training_set:
+        matrix_list.append(lst[:-1])
+        output_list.append(lst[6])
+    output_array = process_output_matrix(output_list)
+    input_array = np.array(matrix_list)
+    return input_array, output_array
+
+
+def process_output_matrix(output_list=[]):
+    out = []
+    for fault in output_list:
+
+        if fault == "A to B to Grd":
+            out.append([1, 1, 0, 1])
+
+        elif fault == 'A to B':
+            out.append([1, 1, 0, 0])
+
+        elif fault == 'C to A':
+            out.append([1, 0, 1, 0])
+
+        elif fault == 'A to Grd':
+            out.append([1, 0, 0, 1])
+
+        elif fault == 'B to C to Grd':
+            out.append([0, 1, 1, 1])
+
+        elif fault == 'B to C':
+            out.append([0, 1, 1, 0])
+
+        elif fault == 'B to Grd':
+            out.append([0, 1, 0, 1])
+
+        elif fault == 'C to Grd':
+            out.append([0, 0, 1, 1])
+
+        elif fault == 'A to C to Grd':
+            out.append([1, 0, 1, 1])
+
+        elif fault == 'no Fault':
+            out.append([0, 0, 0, 0])
+        else:
+            out.append([1, 1, 1, 1])
+    return np.array(out)
+
+
+def check_accuracy(results, output):
+    accuracy = 0
+    output_length = len(output)
+    for i in range(output_length):
+        k = 0
+        for j in range(len(output[i])):
+            if results[i][j] == output[i][j]:
+                k += 1
+            if k == 4:
+                accuracy += 1
+    print('total no of inputs : ', output_length)
+    print("Accurately predicted : ", accuracy)
+    return (accuracy/output_length)*100
 
 if __name__ == '__main__':
+
+    training, testing, classes = util.file_reader()
+    input_matrix1, output_matrix1 = get_matrices(testing)
+
     input_matrix = np.array([[0.997, 0.9991, 0.9985, 0.9978, 0.9988, 0.9984],
                              [0.334, 1.194, 1.172, 3.335, 0.981, 0.979],
                              [1.172, 0.334, 1.194, 0.981, 3.335, 0.979],
@@ -29,9 +100,17 @@ if __name__ == '__main__':
     ann_training = TrainingNetwork()
     y, sn0, sn1 = ann_training.training_network(input_matrix, output_matrix)
     yy = np.round(y, 2)
+    print("weights from 1st layer to hidden layer:")
+    print(sn0)
+    print("weights from hidden layer to output layer:")
+    print(sn1)
 
-    test_data = np.array([1.172, 0.334, 1.194, 0.981, 3.335, 0.979])
+    test_data = input_matrix1   # np.array([1.188, 0.205, 0.205, 0.985, 7.187, 7.855])    # B to C to Grd
     test_service = Testing()
-    test_service.testing_network(test_data, sn0, sn1)
+    result = test_service.testing_network(test_data, sn0, sn1)
+    result = np.round(result)
 
-    print(yy)
+    accur = check_accuracy(result, output_matrix1)
+    print(accur)
+
+
---
+++ util.py
@@ -0,0 +1,38 @@
+import os
+import csv
+
+
+def file_reader():
+    root_path = 'data/'
+    # top_view file_names
+    file_names = [file_name for file_name in os.listdir(root_path)]
+    file_paths = [root_path + file_path for file_path in file_names]
+
+    # Fault types
+    class_titles = [file_name[:-4] for file_name in file_names]
+
+    for file_path in file_paths:
+
+        training, testing = load_data_set(file_path, 0.50)
+
+    return training, testing, class_titles
+
+
+def load_data_set(filename, split, training_set=[], testing_set=[]):
+
+    with open(filename) as csvfile:
+        lines = csv.reader(csvfile)
+        dataset = list(lines)
+        iterator = 0
+        for x in range(len(dataset) - 1):
+            for y in range(len(dataset[x])-1):
+                dataset[x][y] = float(dataset[x][y])
+            if iterator < split*lines.line_num:
+                training_set.append(dataset[x])
+            else:
+                testing_set.append(dataset[x])
+            iterator += 1
+    return training_set, testing_set
+
+if __name__ == "__main__":
+    file_reader()
